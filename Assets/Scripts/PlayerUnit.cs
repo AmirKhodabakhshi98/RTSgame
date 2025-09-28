bc3e4ea0d33d8fa72488d0180aca5aed1ebc9853
Amir Khodabakhshi <62739801+AmirKhodabakhshi98@users.noreply.github.com>
2025-09-28 11:56:14 +0200

    click move works

--- Assets/Scripts/PlayerUnit.cs
+++ Assets/Scripts/PlayerUnit.cs
@@ -1,30 +1,53 @@
+using Pathfinding;
 using UnityEngine;
 
 public class PlayerUnit : MonoBehaviour
 {
+    
+    private AIDestinationSetter ads;
+    private Transform target;
+    private SpriteRenderer sr;
+    private bool selected = false;
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        ads = GetComponent<AIDestinationSetter>();
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //if units start moving weirdly on spawn look here XD
+        target = new GameObject("ClickMarker").transform;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (selected)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mouseWorldPos.z = 0f;
+                target.position = mouseWorldPos;
+                ads.target = target;
+
+            }
+        }
         
     }
     
-    private SpriteRenderer sr;
 
-    private void Awake()
-    {
-        sr = GetComponent<SpriteRenderer>();
-    }
+
+
 
     [SerializeField] private GameObject SelectedEffect;
 
     public void SetSelected(bool isSelected)
     {
-        SelectedEffect.SetActive(isSelected);
+        selected = isSelected;
+        SelectedEffect.SetActive(selected);
     }
 }
